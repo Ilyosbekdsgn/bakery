864ed7a26e3aad87147607fff43275ed7884f7f8
Ilyosbekdsgn <140714881+Ilyosbekdsgn@users.noreply.github.com>
2026-04-10 23:50:02 +0500

    Add files via upload

--- states.py
+++ states.py
@@ -1,10 +1,14 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class OrderState(StatesGroup):
-    waiting_for_order = State()
+    waiting_for_product_quantity = State()
     waiting_for_location_or_address = State()
     waiting_for_phone = State()
     waiting_for_receipt = State()
 
 class AdminState(StatesGroup):
-    waiting_for_price = State()
+    waiting_for_product_photo = State()
+    waiting_for_product_name = State()
+    waiting_for_product_price_whole = State()
+    waiting_for_product_price_slice = State()
+    waiting_for_new_admin_id = State()
