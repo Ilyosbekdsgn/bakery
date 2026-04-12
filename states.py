0d0b2c83d4d80076455b78e1b84dd7285bdb8822
Ilyosbekdsgn <140714881+Ilyosbekdsgn@users.noreply.github.com>
2026-04-12 14:45:39 +0500

    Add files via upload

--- states.py
+++ states.py
@@ -5,6 +5,7 @@ class OrderState(StatesGroup):
     waiting_for_location_or_address = State()
     waiting_for_phone = State()
     waiting_for_receipt = State()
+    waiting_for_custom_order_details = State()
 
 class AdminState(StatesGroup):
     waiting_for_product_photo = State()
@@ -13,3 +14,4 @@ class AdminState(StatesGroup):
     waiting_for_product_price_slice = State()
     waiting_for_new_admin_id = State()
     waiting_for_del_admin_id = State()
+    waiting_for_custom_order_price = State()
